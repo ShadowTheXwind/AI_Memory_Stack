3dcf6358172b8fb67fa5153762acd86ce5542c44
ShadowTheXwind <godsmack.killer654@gmail.com>
2025-08-02 00:27:05 -0700

    Create glyphs.js
    Glyphs

---
+++ glyphs.js
@@ -0,0 +1,11 @@
+<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+  <polygon points="50,10 90,90 10,90" 
+           stroke="cyan" stroke-width="2" 
+           fill="none">
+    <animate attributeName="stroke-opacity" values="1;0.5;1" dur="2s" repeatCount="indefinite" />
+  </polygon>
+</svg>
+- 🧠 Mind
+- 💧 Loss
+- 🔥 Passion
+- 🕯️ Ritual You can create a glyph for each and animate them with matching sound when hovered or scrolled into view.
